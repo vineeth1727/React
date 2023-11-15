746add0aee8fbd9e96d865626a9206cf08b93caf
vineeth1727 <146938755+vineeth1727@users.noreply.github.com>
2023-11-15 12:43:16 +0530

    Add files via upload
    Creating a new component and a file inside it, changing the root module to the class name created and giving an output.

---
+++ vk.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Vk extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>Creating app and component in react </h1>);
+    }
+}
+ 
+export default Vk;
